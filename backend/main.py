4004b0f72394b7b38b1dfaeb9e6aa9c9eed7bad1
Swapnil <swapnilsurywanshi90@gmail.com>
2026-07-23 13:34:58 +0530

    Remove Windows-only dependency

--- backend/main.py
+++ backend/main.py
@@ -53,13 +53,14 @@ Base.metadata.create_all(bind=engine)
 
 app.include_router(auth_router)
 
+FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        FRONTEND_URL,
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5174",
     ],
     allow_credentials=True,
     allow_methods=["*"],
